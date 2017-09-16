8e21a4e68028c88d88c27d8cb10aacb30dc3769c
looups <looupsing@gmail.com>
2017-09-16 10:17:58 +0200

    mise en place des animations

---
+++ PROJET_COWORKING/js/script.js
@@ -0,0 +1,69 @@
+function initMap() {
+  var laloupe = { lat: 48.473762, lng: 1.011784 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: laloupe
+  });
+  var icons = {
+    point: {
+      icon: 'poop.png',
+    }
+  };
+  var features = [
+    {
+      position: new google.maps.LatLng(48.473762, 1.011784),
+      type: 'point'
+    }];
+  features.forEach(function (feature) {
+    var marker = new google.maps.Marker({
+      position: feature.position,
+      icon: icons[feature.type].icon,
+      map: map
+    });
+  });
+}
+/* défilement sur la page SCROLL SMOOTH */
+
+$(document).ready(function () {
+  $('.js-scrollTo').on('click', function () {
+    var page = $(this).attr('href');
+    var speed = 750;
+    $('html, body').animate({ scrollTop: $(page).offset().top - 50 }, speed);
+    return false;
+  });
+});
+
+/* modification de la barre de Navigation */
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    var wn = $(window).scrollTop();
+    if (wn > 120) {
+      $(".barreNavColor").css("background", "rgba(255,255,255,1)");
+    }
+    else {
+      $(".barreNavColor").css("background", "rgba(255,255,255,0)");
+    }
+  });
+});
+/* effet hide show sur section*/
+$(function () {
+  $(".effetHi").hide()
+  $(".espace").hover(function () {
+    $(".effetHi").show()
+  })
+
+});
+$(function () {
+  $(".effetHi1").hide()
+  $(".service").hover(function () {
+    $(".effetHi1").show()
+  })
+
+});
+$(function () {
+  $(".effetHi2").hide()
+  $(".tarifs").hover(function () {
+    $(".effetHi2").show()
+  })
+
+});
